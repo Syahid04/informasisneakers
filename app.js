0afb09f8a90cc7369a471eaf88c62db317883068
Arifin Nur Sahid <146154558+Syahid04@users.noreply.github.com>
2025-06-11 14:48:04 +0700

    Update app.js

--- app.js
+++ app.js
@@ -14,22 +14,27 @@
 // // Fungsi tambahan bisa dimasukkan di sini jika diperlukan
 
 
-registerSW();
+registerSW(); // Memanggil fungsi untuk mendaftarkan service worker
 
-// Registers a service worker
+// Fungsi untuk mendaftarkan service worker
 async function registerSW() {
+  // Mengecek apakah browser mendukung Service Worker
   if ('serviceWorker' in navigator) {
     try {
-      // Change the service worker URL to see what happens when the SW doesn't exist
+      // Mencoba untuk mendaftarkan service worker dengan file "sw.js"
+      // Anda bisa mengubah nama file untuk melihat apa yang terjadi jika file tidak ditemukan
       const registration = await navigator.serviceWorker.register("sw.js");       
     } catch (error) {
+      // Menangani kesalahan jika pendaftaran gagal
       showResult("Error while registering: " + error.message);
     }    
   } else {
+      // Jika browser tidak mendukung service worker
       showResult("Service workers API not available");
   }
 }; 
 
+// Fungsi untuk menampilkan hasil ke elemen <output>
 function showResult(text) {
   document.querySelector("output").innerHTML = text;
 }
